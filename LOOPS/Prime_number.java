240f6bcfe406c6835ec7c393adb33de09aaefc15
Abhay Singh Tomar <122097124+Abhay1930@users.noreply.github.com>
2025-01-18 12:56:11 +0530

    Add files via upload

--- LOOPS/Prime_number.java
+++ LOOPS/Prime_number.java
@@ -1,14 +1,22 @@
 package LOOPS;
-
 import java.util.Scanner;
-
 public class Prime_number {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
         int n = sc.nextInt();
-        for (int i = 0; i < n ; i++){
-            
-            
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                count++;
+            }
+        }
+
+        if (count == 2) {
+            System.out.println("Prime Number");
+        } else {
+            System.out.println("Not A Prime Number");
         }
     }
 }
